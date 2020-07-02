8e3165ca0b7c761a4ddf3897f753c8dfddf3f4bd
Hachem Said <labsar.ahmed@outlook.fr>
2020-07-02 18:42:05 -0400

    created arrays of diffrent characters types

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// pools of different type of characters user can choose from
+var charactersArray = "!@#$%^&*()".split("");
+var upperLettersArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowerLettersArray = "abcdefghijklmnopqrstuvwxyz".split("");
+var numbersArray = "1234567890".split("");
 
+//function returning a password as value and following the users criterias
+function generatePassword() { }
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
