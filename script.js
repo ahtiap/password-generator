6c200fcbf810971fe3f8331f4b356be595f39d58
Hachem Said <labsar.ahmed@outlook.fr>
2020-07-02 22:46:50 -0400

    update js code with checkbox checking

--- script.js
+++ script.js
@@ -20,89 +20,89 @@ function generatePassword() {
   var passwordNew = [];
   // big pool will contain all the pools the user chose to get characters from
   var bigPool = [];
-  // prompt the user to choose the length of the password
+  
+
+  //make sure the user chooses at least one type of characters to include
+  // get checkbox status from the DOM
+  var numbers = document.getElementById("num").checked;
+  var upCase = document.getElementById("upCase").checked;
+  var lowCase = document.getElementById("lowCase").checked;
+  var spcChar = document.getElementById("spcChar").checked;
+
+  //make sure the user has checked at least one box to generate a password
+  if (numbers || upCase || lowCase || spcChar) {
+    // prompt the user to choose the length of the password
   var message = "please pick a length for the password";
   //variable to store password length
   var passLength;
   // loop to make sure user chooses between 8 and 128 characters
   do {
     //call the function promptCheck I created to check user entry and return a valid value
-    alert("Password can't be less than 8 characters long or more than 128 characters");
-    passLength= promptCheck(passLength, message);
-  // console.log(passLength);
-  } while (passLength<8 || passLength>128);
-  
-
-  //make sure the user chooses at least one type of characters to include
-  while (!numbers && !upperLetters && !lowerLetters && !characters) {
-    var numbers = confirm("Would you like to include numbers?");
-    //  console.log("Made a choice about NUMBERS.");
-    var upperLetters = confirm("Would you like to include uppercase letters?");
-    // console.log("Made a choice about UPPER LETTERS.");
-    var lowerLetters = confirm("Would you like to include lowercase letters?");
-    // console.log("Made a choice about LOWER LETTERS.");
-    var characters = confirm("Would you like to include special characters?");
-    if (!numbers && !upperLetters && !lowerLetters && !characters) {
-      alert("you have to pick at least one criteria");
+    alert(
+      "Password has to be at least 8 characters long and no more than 128 characters"
+    );
+    passLength = promptCheck(passLength, message);
+    // console.log(passLength);
+  } while (passLength < 8 || passLength > 128);
+    
+    // if selected make sure at least one number is in the mix
+    if (numbers) {
+      passwordNew.push(
+        numbersArray[Math.floor(Math.random() * numbersArray.length)]
+      );
+      //update password length
+      passLength--;
+      // add the numbers to the big pool
+      bigPool = bigPool.concat(numbersArray);
+      //console.log(bigPool);
     }
-  }
-  //variable to store the new password
-  var passwordNew = [];
-  // big pool will contain all the pools the user chose to get characters from
-  var bigPool = [];
-  // if selected make sure at least one number is in the mix
-  if (numbers) {
-    passwordNew.push(
-      numbersArray[Math.floor(Math.random() * numbersArray.length)]
-    );
-    //update password length
-    passLength--;
-    // add the numbers to the big pool
-    bigPool = bigPool.concat(numbersArray);
+    // if selected make sure at least one uppercase letter is in the mix
+    if (upCase) {
+      passwordNew.push(
+        upperLettersArray[Math.floor(Math.random() * upperLettersArray.length)]
+      );
+      //update password length
+      passLength--;
+      // add the uppercase letters to the big pool
+      bigPool = bigPool.concat(upperLettersArray);
+      //console.log(bigPool);
+    }
+    // if selected make sure at least one lowercase is in the mix
+    if (lowCase) {
+      passwordNew.push(
+        lowerLettersArray[Math.floor(Math.random() * lowerLettersArray.length)]
+      );
+      //update password length
+      passLength--;
+      // add the lower letters  to the big pool
+      bigPool = bigPool.concat(lowerLettersArray);
+      //console.log(bigPool);
+    }
+    // if selected make sure at least one special character is in the mix
+    if (spcChar) {
+      passwordNew.push(
+        charactersArray[Math.floor(Math.random() * charactersArray.length)]
+      );
+      //update password length
+      passLength--;
+      // add the special characters  to the big pool
+      bigPool = bigPool.concat(charactersArray);
+      //console.log(bigPool);
+    }
+    // a pool to fill the rest of the password
     //console.log(bigPool);
+    for (let i = 0; i < passLength; i++) {
+      // add a random character at each iteration
+      passwordNew.push(bigPool[Math.floor(Math.random() * bigPool.length)]);
+      //console.log(passLength);
+    }
+    //return password value
+    return passwordNew.join("");
   }
-  // if selected make sure at least one uppercase letter is in the mix
-  if (upperLetters) {
-    passwordNew.push(
-      upperLettersArray[Math.floor(Math.random() * upperLettersArray.length)]
-    );
-    //update password length
-    passLength--;
-    // add the uppercase letters to the big pool
-    bigPool = bigPool.concat(upperLettersArray);
-    //console.log(bigPool);
+  //otherwise let them now in the text box
+  else {
+    return "You have to choose at least one criteria";
   }
-  // if selected make sure at least one lowercase is in the mix
-  if (lowerLetters) {
-    passwordNew.push(
-      lowerLettersArray[Math.floor(Math.random() * lowerLettersArray.length)]
-    );
-    //update password length
-    passLength--;
-    // add the lower letters  to the big pool
-    bigPool = bigPool.concat(lowerLettersArray);
-    //console.log(bigPool);
-  }
-  // if selected make sure at least one special character is in the mix
-  if (characters) {
-    passwordNew.push(
-      charactersArray[Math.floor(Math.random() * charactersArray.length)]
-    );
-    //update password length
-    passLength--;
-    // add the special characters  to the big pool
-    bigPool = bigPool.concat(charactersArray);
-    //console.log(bigPool);
-  }
-  // a pool to fill the rest of the password
-  //console.log(bigPool);
-  for (let i = 0; i < passLength; i++) {
-    // add a random character at each iteration
-    passwordNew.push(bigPool[Math.floor(Math.random() * bigPool.length)]);
-    //console.log(passLength);
-  }
-  //return password value
-  return passwordNew.join("");
 }
 
 // function to check the user input
